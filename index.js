061e9dfc4ce049c5b2eeb9da4dd946433994d5c9
Afia <sanjidaakterafia0307@gmail,com>
2022-11-08 02:31:47 +0600

    service data

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT 
-
+require("dotenv").config()
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3jlrk4o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const serviceCollection=client.db("foodie").collection("services")
+
+    }
+    finally{
+
+    }
+}
+run().catch(er=>console.error(er))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
