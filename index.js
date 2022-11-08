f8e6ad9f88fb12710172f9841d785be44ad0741b
Afia <sanjidaakterafia0307@gmail,com>
2022-11-08 15:15:29 +0600

    service done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port = process.env.PORT 
+const port = process.env.PORT || 5000 
 require("dotenv").config()
 app.use(cors())
 app.use(express.json())
@@ -14,7 +14,33 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection=client.db("foodie").collection("services")
+        app.get("/services",async(req,res)=>{
+          const count=parseInt(req.query.count);
+          console.log(count)
+          if(count){
 
+            const query={}
+            const cursor= serviceCollection.find(query)
+            const services=await cursor.limit(3).toArray()
+            res.send(services)
+          }
+          else{
+            const query={}
+            const cursor= serviceCollection.find(query)
+            const services=await cursor.toArray()
+            res.send(services)
+
+          }
+        })
+
+        app.get("/services/:id",async(req,res)=>{
+          const id=req.params.id;
+
+          const query={_id:ObjectId(id)}
+
+          const service = await serviceCollection.findOne(query)
+          res.send(service)
+        })
     }
     finally{
 
